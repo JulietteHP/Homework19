d2abd4ce8e7d6190fe3c4079b21495af357aacda
Juliette Hartel <64052793+JulietteHP@users.noreply.github.com>
2023-06-19 16:52:31 -0400

    Homework 19 ready for user testing
    This is ready for user testing though I'd like to make the containers the same height on the portfolio page.
    Needs better images for the portfolio pages.

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+// Step 4: Animate a DIV
+
+  $(document).ready(function() {
+     $('.animate').animate( {left: '450px'}, 1000 );
+});
+
+//$("div").mouseleave(function(){
+ // alert('Thanks for stopping by. Come back soon for more content.'); });
+
+// $("div").mouseenter(function(){
