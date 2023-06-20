9b5539dd0433c96311e5e0fe3d0c7e75df876c33
Juliette Hartel <64052793+JulietteHP@users.noreply.github.com>
2023-06-19 21:55:49 -0400

    minor fixes

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 // Step 4: Animate a DIV
 
   $(document).ready(function() {
-     $('.animate').animate( {left: '675px'},  3000 );
+     $('.animate').animate( {left: '50%'},  3000 );
 });
 //$("div").mouseleave(function(){
  // alert('Thanks for stopping by. Come back soon for more content.'); });
