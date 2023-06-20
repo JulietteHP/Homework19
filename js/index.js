0b46a280a06fefadcb7d1ea0ce20e2f06aaa3567
Juliette Hartel <64052793+JulietteHP@users.noreply.github.com>
2023-06-19 21:28:13 -0400

    changes after user testing
    Changes made after user testing. fixed animation.

--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@
 // Step 4: Animate a DIV
 
   $(document).ready(function() {
-     $('.animate').animate( {left: '450px'}, 1000 );
+     $('.animate').animate( {left: '675px'},  3000 );
 });
-
 //$("div").mouseleave(function(){
  // alert('Thanks for stopping by. Come back soon for more content.'); });
 
 // $("div").mouseenter(function(){
+0
